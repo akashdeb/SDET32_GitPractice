a8381ca5e635ef7a93d58c5b8a08ea278112798e
Akash <Akash@TYC1121225>
2022-04-12 12:15:50 +0530

    Commit 2

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassB.java
@@ -0,0 +1,13 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+	
+	@Test
+	public void demoTest2() {
+		
+		System.out.println("Push 2");
+	}
+
+}
