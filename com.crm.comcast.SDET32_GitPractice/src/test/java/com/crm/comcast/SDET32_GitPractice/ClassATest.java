e72e042796eb1db0b3a175d04ad9fc07a04f6871
Akash <Akash@TYC1121225>
2022-04-14 12:02:26 +0530

    Commit 9

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassATest.java
@@ -0,0 +1,20 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassATest {
+	
+	@Test
+	public void demoTest() {
+		String browser = System.getProperty("browser");
+		String url = System.getProperty("url");
+		
+		System.out.println(browser);
+		System.out.println(url);
+
+
+		System.out.println("First Push");
+		
+	}
+
+}
